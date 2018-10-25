cc1b0164770ab9907e962927dfa3e2c2186d7f50
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-10-25 21:38:32 +0300

    added email and password validity checks

--- app/frontend/robin/src/pages/register/index.js
+++ app/frontend/robin/src/pages/register/index.js
@@ -17,13 +17,16 @@ export default class Login extends React.Component {
       username:"",
       password: "",
       repeatPasswprd: "",
-      acceptedTerms: false
+      acceptedTerms: false,
+      formHasErrors: true
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
-  handleChange(e) {
+  async handleChange(e) {
     const { name, value } = e.target
   
     this.setState({[name] : value,});
@@ -37,8 +40,52 @@ export default class Login extends React.Component {
 
   }
 
+  validatePassword(){
+    if(this.state.password === ""){
+      return (
+        <input type="password" className="form-control" placeholder="Password" required name="password" value={this.state.password} onChange={this.handleChange}/>
+      );
+    }
+    else if(this.state.password.length < 8 || this.state.password.length > 20){
+      return (
+        <input type="password" className="form-control is-invalid" placeholder="Password" required name="password" value={this.state.password} onChange={this.handleChange}/>
+      );
+    }
+    else {
+      return (
+        <input type="password" className="form-control is-valid" placeholder="Password" required name="password" value={this.state.password} onChange={this.handleChange}/>
+      );
+    }
+  }
+
+  validateEmail(){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(this.state.email === ""){
+      return(
+        <input type="text" className="form-control" placeholder="Email" required autofocus name="email" value={this.state.email} onChange={this.handleChange}/>
+      );
+    }
+    else if(re.test(String(this.state.email).toLowerCase())){
+      return(
+        <input type="text" className="form-control is-valid" placeholder="Email" required autofocus name="email" value={this.state.email} onChange={this.handleChange}/>
+      );
+    }
+    else{
+      return(
+        <input type="text" className="form-control is-invalid" placeholder="Email" required autofocus name="email" value={this.state.email} onChange={this.handleChange}/>
+      );
+    }
+  }
+  
+  checkFormErrors(){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailValidity = re.test(String(this.state.email).toLowerCase());
+    var passwordValidity = (this.state.password.length < 20) && (this.state.password.length > 8)
+    return emailValidity && passwordValidity;
+  }
+
   enableButton(){
-    if(this.state.email === "" || this.state.password === "" || this.state.name === "" || this.state.username === "" || this.state.acceptedTerms === false)
+    if(this.state.email === "" || this.state.password === "" || this.state.name === "" || this.state.username === "" || this.state.acceptedTerms === false || !this.checkFormErrors())
       return (
         <button className="btn btn-lg btn-primary btn-block button-disabled" disabled type="submit">
           Sign up
@@ -64,9 +111,9 @@ export default class Login extends React.Component {
               <h2 className="text-center">Robin</h2>
               <form className="form-signup">
                 <input type="text" className="form-control" placeholder="Full Name" required autofocus name="name" onChange={this.handleChange}/>
-                <input type="text" className="form-control" placeholder="Email" required autofocus name="email" onChange={this.handleChange}/>
                 <input type="text" className="form-control" placeholder="Username" required autofocus name="username" onChange={this.handleChange}/>
-                <input type="password" className="form-control" placeholder="Password" required name="password" onChange={this.handleChange}/>
+                {this.validateEmail()}
+                {this.validatePassword()}
                 <div className="terms">
                   <input type="checkbox" onChange={this.handleCheckboxChange} onClick={() => {this.checked = !this.checked}}/> I accept terms and conditions
                 </div>

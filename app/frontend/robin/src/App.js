9a23f06f3454d546f1da13bdee9e5f82b9270ff3
ugurcanarikan <ugurcan@UA-Mac.local>
2018-10-25 14:57:00 +0300

    added bootstrap login page

--- app/frontend/robin/src/App.js
+++ app/frontend/robin/src/App.js
@@ -4,15 +4,18 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import SignIn from "./pages/signIn/index"
 import SignUp from "./pages/signUp/index"
 import ForgotPassword from "./pages/forgotPW/index"
+import Main from "./pages/main/index"
+import Login from "./pages/login/index"
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <Redirect to={"/signIn"} />} />
-        <Route path="/signIn" component={SignIn} />
+        <Route path="/login" component={Login} />
         <Route path="/signUp" component={SignUp} />
         <Route path="/forgotPassword" component={ForgotPassword} />
+        <Route path="/main" component={Main} />
       </Switch>
     );
   }
---
+++ app/frontend/robin/src/pages/main/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import AppBar from "../components/appBar/index"
+
+export default () => {return (<AppBar/>)}

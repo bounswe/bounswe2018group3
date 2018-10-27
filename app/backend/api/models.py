4726d8e26859482c49a3a92ac5f2b36a12eeb993
OmegaIV <onur_vark@hotmail.com>
2018-10-27 18:32:51 +0300

    Tag model added

--- app/backend/api/models.py
+++ app/backend/api/models.py
@@ -9,14 +9,19 @@ class Event(models.Model):
     artist = models.CharField(max_length=255)
     date = models.DateField()
     price = models.FloatField()
-    #tags
-    #comments
+    #tags =
+    #comments =
     rating = models.DecimalField(max_digits=3,decimal_places=2)
-    #images
-    #attendants
+    #images = 
+    #attendants =
 
 class Comment(models.Model):
     author = models.CharField(max_length=255)
     rating = models.DecimalField(max_digits=3,decimal_places=2)
     date = models.DateField()
     content = models.TextField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    #connectedTags =
+    followerNumber = models.IntegerField()

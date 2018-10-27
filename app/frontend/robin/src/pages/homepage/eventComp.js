616d9f7e9877434a2605cb88009b35a01d6ea20d
Recep Deniz Aksoy <recepdeniza@gmail.com>
2018-10-27 17:19:11 +0300

    initial commit for event component

---
+++ app/frontend/robin/src/pages/homepage/eventComp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+const Example = (props) => {
+  return (
+    <Row>
+      <Col sm="6">
+      <Card>
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+        </CardBody>
+        <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardBody>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <CardLink href="#">Card Link</CardLink>
+          <CardLink href="#">Another Link</CardLink>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default Example;

bbd8e66bed4374983d5734c536df09634bd8d683
Onur Varkıvanç <onurvark@Drop.local>
2018-10-27 18:54:09 +0300

    Returned to using AbstractUser

--- app/backend/users/models.py
+++ app/backend/users/models.py
@@ -1,21 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.core import validators
 
-
-
-class CustomUser(User):
+class CustomUser(AbstractUser):
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255) #check at least 1 number etc.
     bio = models.TextField(blank=True)
-    email = models.CharField(validators=[validators.EmailValidator])
+    email = models.CharField(max_length=255, validators=[validators.EmailValidator])
     username = models.CharField(max_length=255, unique=True)
-    photo = models.ImageField()
+    #photo = models.ImageField()
     #colorScheme
     #watchingTags
     #followedUsers
     #commentList
-    rating = models.DecimalField(max_digits=3,decimal_places=2)
+    rating = models.DecimalField(max_digits=3,decimal_places=2,default=0)
     #eventList
     #blockedUsers
     #blockedTags

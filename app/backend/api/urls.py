60534e2faa80b9c9f1220b87a282e20b72711c6c
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 19:07:04 +0300

    fixed some names

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -3,14 +3,14 @@ from .views import FacebookLogin, ExampleView
 from allauth.account.views import ConfirmEmailView
 from allauth.account.views import confirm_email as allauthemailconfirmation
 from rest_auth.registration.views import VerifyEmailView
-
+from views import complete_view
 
 urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view(), name='account_confirm_email'),
-    path('registration/complete/', views.complete_view, name='account_confirm_complete'),
+    path('registration/complete/', complete_view, name='account_confirm_complete'),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('rest-auth/ExampleView/', ExampleView.as_view()),
     #re_path(r'^account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),

ffcb985fe554448e8c7480977fd1670e232218ec
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 17:17:04 +0300

    django.auth.urls added to url for testing email confirmation

--- app/backend/backend/urls.py
+++ app/backend/backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path("", include('django.contrib.auth.urls')),
 ]

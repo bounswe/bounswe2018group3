0a59f8d4de9c01432c38bd8d4d4433fad83ac656
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 18:30:30 +0300

    added some code I hope this works... Darn Email confirmation

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path, re_path
 from .views import FacebookLogin, ExampleView
+from allauth.account.views import ConfirmEmailView
 from allauth.account.views import confirm_email as allauthemailconfirmation
 from rest_auth.registration.views import VerifyEmailView
 
@@ -8,9 +9,9 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('registration/account-confirm-email/<str:key>/', allauthemailconfirmation, name="account_confirm_email"),
+    path('registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view(), name="account_confirm_email"),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('rest-auth/ExampleView/', ExampleView.as_view()),
-    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(), name='account_confirm_email'),
+    #re_path(r'^account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    #re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(), name='account_confirm_email'),
 ]

4314990e1b15965f487e9cb292be2e1fc5d50e76
ugurcanarikan <ugurcan@UA-Mac.local>
2018-10-27 17:06:40 +0300

    added forgotpasswordsuccess page, added redirect for login and forgotpassword, disabled redux store

--- app/frontend/robin/src/App.js
+++ app/frontend/robin/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 
-import ForgotPassword from "./pages/forgotpassword/index"
-import Register from "./pages/register/index"
-import Login from "./pages/login/index"
-import HomePage from "./pages/homepage/index"
+import ForgotPassword from "./pages/forgotpassword/index";
+import Register from "./pages/register/index";
+import Login from "./pages/login/index";
+import HomePage from "./pages/homepage/index";
+import ForgotPasswordSuccess from "./pages/forgotpasswordsuccess/index";
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Route exact path="/" render={() => <Redirect to={"/register"} />} />
         <Route path="/login" component={Login} />
         <Route path="/forgotPassword" component={ForgotPassword} />
+        <Route path="/forgotPasswordSuccess" component={ForgotPasswordSuccess} />
         <Route path="/register" component={Register} />
         <Route path="/home" component={HomePage} />
       </Switch>

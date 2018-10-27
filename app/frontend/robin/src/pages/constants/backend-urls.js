15683daed22140120105c412d13e54f7851f21a1
ugurcanarikan <ugurcan@UA-Mac.local>
2018-10-27 16:05:48 +0300

    added forgot password url

--- app/frontend/robin/src/pages/constants/backend-urls.js
+++ app/frontend/robin/src/pages/constants/backend-urls.js
@@ -1,2 +1,3 @@
 export const LOGIN_URL = "http://139.59.128.92:8080/api/v1/rest-auth/login/";
 export const REGISTRATION_URL = "http://139.59.128.92:8080/api/v1/rest-auth/registration/";
+export const FORGOT_PASSWORD_URL = "";

b3e5966ef5e22d8e77c9fc84cc308f5e71d5287f
Onur Varkıvanç <onurvark@Drop.local>
2018-10-27 18:15:06 +0300

    Basic skeleton of models added

--- app/backend/api/models.py
+++ app/backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    info = models.TextField(blank=True)
+    location = models.TextField() #subject to change
+    artist = models.CharField(max_length=255)
+    date = models.DateField()
+    price = models.FloatField()
+    #tags
+    #comments
+    rating = models.DecimalField(max_digits=3,decimal_places=2)
+    #images
+    #attendants
+
+class Comment(models.Model):
+    author = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=3,decimal_places=2)
+    date = models.DateField()
+    content = models.TextField()
--- app/backend/users/models.py
+++ app/backend/users/models.py
@@ -1,10 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.core import validators
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    name = models.CharField(blank=True, max_length=255)
+class CustomUser(User):
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255) #check at least 1 number etc.
+    bio = models.TextField(blank=True)
+    email = models.CharField(validator=[validators.EmailValidator])
+    username = models.CharField(max_length=255, unique=True)
+    photo = models.ImageField()
+    #colorScheme
+    #watchingTags
+    #followedUsers
+    #commentList
+    rating = models.DecimalField(max_digits=3,decimal_places=2);
+    #eventList
+    #blockedUsers
+    #blockedTags
 
     def __str__(self):
         return self.email

86649418344d92cef5e1b0fdb46f2932377b69e7
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 14:51:16 +0300

    comment-out tokenAuthentication in order to activate jwt

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -152,7 +152,7 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
+       #'rest_framework.authentication.TokenAuthentication',
        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (

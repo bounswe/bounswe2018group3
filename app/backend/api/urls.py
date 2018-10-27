3602bdccb76ec03de2c3e607070b2218d38bfc63
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 16:59:42 +0300

    import url added :)

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.conf.urls import url
 from .views import FacebookLogin, ExampleView
 from allauth.account.views import confirm_email as allauthemailconfirmation
 
@@ -6,7 +7,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('rest-auth/registration'),
     url(r'^rest-auth/registration/account-confirm-email/(?P<key>\w+)/$', allauthemailconfirmation, name="account_confirm_email"),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('rest-auth/ExampleView/', ExampleView.as_view()),

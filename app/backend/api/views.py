5f8cfb2d6b11b9b664dbebc15faa935ff7cbd90f
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 13:44:49 +0300

    issue #86 jwt etc.

--- app/backend/api/views.py
+++ app/backend/api/views.py
@@ -4,6 +4,23 @@ from allauth.socialaccount.providers.facebook.views import \
 from django.shortcuts import render
 from rest_auth.registration.views import SocialLoginView
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
+
+class ExampleView(APIView):
+    # authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return Response(content)

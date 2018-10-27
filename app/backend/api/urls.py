b05f8965c970fc67e3fb5bc177248311e052b9c8
pxfurkanenes <furkanyaln9@gmail.com>
2018-10-27 16:55:12 +0300

    trying to fix email confirmation

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from .views import FacebookLogin, ExampleView
+from allauth.account.views import confirm_email as allauthemailconfirmation
 
 urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/registration')
+    url(r'^rest-auth/registration/account-confirm-email/(?P<key>\w+)/$', allauthemailconfirmation, name="account_confirm_email"),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('rest-auth/ExampleView/', ExampleView.as_view()),
 

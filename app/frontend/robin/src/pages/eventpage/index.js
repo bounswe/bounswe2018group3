a3b114eb8f34c4daa86a7418c2a018b8fdb2800d
ozgedincsoy <ozge.dincsoy@boun.edu.tr>
2018-10-27 16:49:57 +0300

    event page is updated, rating and price fields are added

--- app/frontend/robin/src/pages/eventpage/index.js
+++ app/frontend/robin/src/pages/eventpage/index.js
@@ -1,8 +1,12 @@
 import Event from "./event"
+import Navbar from "../homepage/navbar"
 import React from 'react';
 
 export default (props) => {
   return (
+    <React.Fragment>
+    <Navbar />
       <Event />
+    </React.Fragment>
   );
 };

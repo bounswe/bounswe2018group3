d43017cc2e0e6e5a5229ffe92c4b50a8191c8c9d
Onur Varkıvanç <onurvark@Drop.local>
2018-10-28 20:48:11 +0300

    Indexation added. Pillow import added to pipfile for image fields

--- app/backend/api/models.py
+++ app/backend/api/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.core import validators
 
 class Event(models.Model):
+    #index = models.PositiveIntegerField(unique=True,db_index=True)
     name = models.CharField(max_length=255)
     info = models.TextField(blank=True)
     location = models.TextField() #subject to change
     artist = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     price = models.FloatField()
     tags = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
     comments = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
@@ -15,12 +16,14 @@ class Event(models.Model):
     attendants = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
 
 class Comment(models.Model):
+    #id = models.IntegerField(unique=True,db_index=True)
     author = models.CharField(max_length=255)
     rating = models.DecimalField(max_digits=3,decimal_places=2,default=0)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     content = models.TextField()
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255,unique=True)
+    #id = models.IntegerField(unique=True,db_index=True)
+    name = models.CharField(max_length=255,unique=True,db_index=True)
     connectedTags = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
-    followerNumber = models.IntegerField()
+    followerNumber = models.PositiveIntegerField()

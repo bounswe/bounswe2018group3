0a71b701428f22a5e531d5323c6650119971fa8f
Onur Varkıvanç <onurvark@Drop.local>
2018-10-28 23:32:33 +0300

    Reformed events as a seperate app

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -9,6 +9,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('users/', include('users.urls')),
+    path('events/', include('events.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('rest-auth/registration/account-confirm-email/<str:key>/', TemplateView.as_view(), name='account_confirm_email'),

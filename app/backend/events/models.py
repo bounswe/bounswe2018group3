bdafa1fc20fb6e65eaeac9b1d1c3ad2b5b114a4c
oguzhanyetimoglu <oguzhanyetimoglu@gmail.com>
2018-11-04 16:53:57 +0300

    Update models.py

--- app/backend/events/models.py
+++ app/backend/events/models.py
@@ -5,9 +5,11 @@ class Event(models.Model):
     name = models.CharField(max_length=255)
     info = models.TextField(blank=True)
     location = models.TextField() #subject to change
+    creator = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     date = models.DateField(db_index=True, blank=True, null=True)
-    price = models.FloatField(blank=True, null=True)
+    time = models.TimeField(db_index=True, blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     tags = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
     comments = models.CharField(validators=[validators.int_list_validator],max_length=255,blank=True)
     rating = models.DecimalField(max_digits=3,decimal_places=2,default=0)

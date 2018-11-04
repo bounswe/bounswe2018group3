ae6499795e22df9fb23b6416b62c3747539eae1e
ozgedincsoy <ozge.dincsoy@boun.edu.tr>
2018-11-04 14:29:49 +0300

    reactstrap is deleted from event page

--- app/frontend/robin/src/pages/eventpage/event.js
+++ app/frontend/robin/src/pages/eventpage/event.js
@@ -3,8 +3,6 @@ import "./eventpage.css";
 import borisPhoto from "../homepage/boris.png";
 import React from 'react';
 import StarRatingComponent from 'react-star-rating-component';
-import { Card, CardImg, CardText, CardBody, CardLink,
-  CardTitle, CardSubtitle, Row, Col } from 'reactstrap';
 
 
   export default class Event extends React.Component {
@@ -54,39 +52,6 @@ import { Card, CardImg, CardText, CardBody, CardLink,
           </div>
 
         </div>
-        /** 
-      <Card>
-
-      <CardBody>
-        <CardTitle>Boris Brejcha for Cercle</CardTitle>
-        <CardSubtitle>Château de Fontainebleau, 24 Oct 2018</CardSubtitle>
-      </CardBody>
-      <Row>
-      <Col sm="6">
-      <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-      </Col>
-      <Col sm="6">
-      <CardBody>
-        <Row>
-        <Col sm="9">
-        <CardText>Free</CardText>
-        </Col>
-        <Col sm="3">
-        <StarRatingComponent 
-                  name="rate1" 
-                  starCount={5}
-                  value={rating}
-                  onStarClick={this.onStarClick.bind(this)}
-        />
-        </Col>
-        </Row>
-        <CardText>This is the masterpiece of his musical work so far. His unbelievable passion for music combined with the longtime experience as a producer for different genres fuses into this exclusive distinctive and very special sound.</CardText>
-        <CardLink href="#">Join</CardLink>
-        <CardLink href="#">Send Message</CardLink>
-      </CardBody>
-      </Col></Row>
-      </Card>
-      */
       )
     }
   }

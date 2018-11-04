752efdbec8b376b8a009b81cd6bf88e7bd6044db
ozgedincsoy <ozge.dincsoy@boun.edu.tr>
2018-11-04 14:30:24 +0300

    reactstrap is deleted from comment component

--- app/frontend/robin/src/pages/eventpage/comment.js
+++ app/frontend/robin/src/pages/eventpage/comment.js
@@ -55,40 +55,6 @@ import { Card, CardImg, CardText, CardBody, CardLink,
       </div>
       </div>      
         </div>
-
-        /** 
-      <Card>
-      <Row>
-      <Col sm="1"></Col>
-
-      <Col sm="8">
-      <CardBody>
-        <Row>
-        <Col sm="2">
-        <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        </Col>
-        <Col sm="6">
-        <CardText>User Name</CardText>
-        </Col>
-        <Col sm="3">
-        <StarRatingComponent 
-                  name="rate1" 
-                  starCount={5}
-                  value={rating}
-                  onStarClick={this.onStarClick.bind(this)}
-        />
-        </Col>
-        </Row>
-        <CardText>Good Event.</CardText>
-        <CardLink href="#">Card Link</CardLink>
-        <CardLink href="#">Another Link</CardLink>
-      </CardBody>
-      </Col>
-      <Col sm="1">
-      </Col>
-      </Row>      
-      </Card>
-     */
       )
     }
   }

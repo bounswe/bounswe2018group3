569c526a784ef716977b965c5392306561b59c06
Cagdas Tombul <cagdastombul@hotmail.com>
2018-11-26 20:19:08 +0300

    Layout is added to the adapters.

--- app/android/Robin/app/src/main/java/com/culturalactivities/robin/adapters/EventAdapter.java
+++ app/android/Robin/app/src/main/java/com/culturalactivities/robin/adapters/EventAdapter.java
@@ -23,29 +23,34 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
     private Context context;
     private ArrayList<Event> events;
     private View.OnClickListener onClickListener;
+    private int adapterType;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView tvName, tvDescription;
+        public TextView tvName, tvDescription, tvDate, tvArtist;
         public ImageView ivBanner;
         public RatingBar ratingBar;
         ViewHolder(View v) {
             super(v);
             tvName = v.findViewById(R.id.tvEventName);
+            tvArtist = v.findViewById(R.id.tvArtistName);
+            tvDate = v.findViewById(R.id.tvEventDate);
             ivBanner = v.findViewById(R.id.ivBanner);
             tvDescription = v.findViewById(R.id.tvDescription);
             ratingBar = v.findViewById(R.id.ratingBar);
+
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public EventAdapter(Context context, ArrayList<Event> events, View.OnClickListener onClickListener) {
+    public EventAdapter(Context context, ArrayList<Event> events, View.OnClickListener onClickListener, int adapterType) {
         this.context = context;
         this.events = events;
         this.onClickListener = onClickListener;
+        this.adapterType = adapterType;
     }
 
     // Create new views (invoked by the layout manager)
@@ -56,7 +61,13 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
         LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view;
         assert inflater != null;
-        view = inflater.inflate(R.layout.simple_event, parent, false);
+        if(adapterType==0){
+
+            view = inflater.inflate(R.layout.simple_event, parent, false);
+        }else{
+            view = inflater.inflate(R.layout.simple_user_event, parent, false);
+
+        }
         view.setOnClickListener(onClickListener);
         return new ViewHolder(view);
     }
@@ -69,11 +80,18 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
         // - replace the contents of the view with that element
         Event event = events.get(position);
         holder.tvName.setText(event.getEventName());
-        holder.tvDescription.setText(event.getEventInfo());
-        holder.ratingBar.setRating(event.getRating());
         holder.tvName.setTypeface(MainActivity.ubuntuRegular);
-        holder.tvDescription.setTypeface(MainActivity.ubuntuRegular);
         //Glide.with(context).load(event.getImages().get(0).getUrl()).into(holder.ivBanner);
+
+        if(adapterType==0){
+            holder.ratingBar.setRating(event.getRating());
+            holder.tvDescription.setTypeface(MainActivity.ubuntuRegular);
+            holder.tvDescription.setText(event.getEventInfo());
+        }else{
+            holder.tvDate.setText(event.getDate());
+            holder.tvArtist.setText(event.getArtistInfo());
+        }
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)

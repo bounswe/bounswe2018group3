9f24223578ae3def3a9089a09f0033d562778f66
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-11-27 18:51:06 +0300

    added new error messages to registration page, added terms and conditions page

---
+++ app/frontend/robin/src/pages/terms/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default class Terms extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1 className="text-center">Terms and Conditions</h1>
+        <p className="text-center">I hereby agree to give AA to all members of the CmpE 451 Group 3</p>
+        <Link to="login" className="register-link">
+          <p className="text-center new-account">Go back </p>
+        </Link>
+      </div>
+    )
+  }
+
+
+}

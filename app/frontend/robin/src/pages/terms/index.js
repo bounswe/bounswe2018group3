9f225d3fb557ed60ebe60790532c243dff846628
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-11-27 19:05:53 +0300

    terms finalized

--- app/frontend/robin/src/pages/terms/index.js
+++ app/frontend/robin/src/pages/terms/index.js
@@ -7,10 +7,11 @@ export default class Terms extends React.Component {
       <div>
         <h1 className="text-center">Terms and Conditions</h1>
         <p className="text-center">You hereby agree to give AA to all members of the CmpE 451 Group 3 for the class of CmpE 451 2018-2019 Fall</p>
-        <Link to="login" className="register-link">
+        <Link to="register" className="register-link">
           <p className="text-center new-account">Go back </p>
         </Link>
       </div>
     )
   }
+
 }

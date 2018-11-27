7454514c3da10461a3ad2bdd6de7d27909623287
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-11-27 18:59:21 +0300

    some changes on the terms

--- app/frontend/robin/src/pages/terms/index.js
+++ app/frontend/robin/src/pages/terms/index.js
@@ -6,13 +6,11 @@ export default class Terms extends React.Component {
     return (
       <div>
         <h1 className="text-center">Terms and Conditions</h1>
-        <p className="text-center">I hereby agree to give AA to all members of the CmpE 451 Group 3</p>
+        <p className="text-center">You hereby agree to give AA to all members of the CmpE 451 Group 3 for the class of CmpE 451 2018-2019 Fall</p>
         <Link to="login" className="register-link">
           <p className="text-center new-account">Go back </p>
         </Link>
       </div>
     )
   }
-
-
 }

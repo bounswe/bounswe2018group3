5b3876c10314a3819d5c1d09b22c6f3f799efa19
Mustafa <mustafa.sisik@boun.edu.tr>
2018-12-15 11:01:06 +0300

    SettingsFragment created.

---
+++ app/android/Robin/app/src/main/java/com/culturalactivities/robin/fragments/EditProfileFragment.java
@@ -0,0 +1,74 @@
+package com.culturalactivities.robin.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.culturalactivities.robin.R;
+
+
+public class EditProfileFragment extends Fragment {
+
+
+    private AppCompatActivity activity;
+    @Override
+    public void onAttach(Context context) {
+        activity = (AppCompatActivity) context;
+        super.onAttach(context);
+    }
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static EditProfileFragment newInstance() {
+        EditProfileFragment fragment = new EditProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/android/Robin/app/src/main/java/com/culturalactivities/robin/fragments/SettingsFragment.java
@@ -0,0 +1,62 @@
+package com.culturalactivities.robin.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.culturalactivities.robin.R;
+public class SettingsFragment extends Fragment {
+
+
+    private AppCompatActivity activity;
+    @Override
+    public void onAttach(Context context) {
+        activity = (AppCompatActivity) context;
+        super.onAttach(context);
+    }
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}

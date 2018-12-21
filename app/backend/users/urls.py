07952b271211af6e41a9351ae0cc000b5b6c1021
Onur Varkıvanç <onur_vark@hotmail.com>
2018-12-21 14:13:39 +0300

    Minor correction

--- app/backend/users/urls.py
+++ app/backend/users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('follow/<int:followed_user_id>', views.UserFollowsUserView.as_view({'get': 'follow', 'delete': 'unfollow'})),
     path('block/<int:blocked_user_id>', views.UserBlocksUserView.as_view({'get': 'block', 'delete': 'unblock'})),
     path('watch/<int:tag_id>', views.UserWatchesTagView.as_view({'get': 'watch', 'delete': 'unwatch'})),
-    path('blockTags/<int:tag_id>', views.UserBlocksTagView.as_view({'get': 'block', 'delete': 'unblock'})),
+    path('blockTag/<int:tag_id>', views.UserBlocksTagView.as_view({'get': 'block', 'delete': 'unblock'})),
     path('getpic/<int:user_id>', views.UserPicView.as_view({'get': 'getpic'})),
     path('rate/<int:user_id>/<int:new_rating>', views.UserRateView.as_view({'get': 'rate'})),
     path('flag/<int:user_id>', views.UserFlagView.as_view({'get': 'get', 'post': 'flag', 'delete': 'unflag'})),

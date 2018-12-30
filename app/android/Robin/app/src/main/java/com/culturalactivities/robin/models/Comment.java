47467933e5467f789545d498a1e30aa0cd305246
Cagdas Tombul <cagdastombul@hotmail.com>
2018-12-30 23:36:04 +0300

    Event comments are now available for each event if exist. However, users and ratings are not added. For users, an enhancement is requested from the backend. Ratings will be handled later.

--- app/android/Robin/app/src/main/java/com/culturalactivities/robin/models/Comment.java
+++ app/android/Robin/app/src/main/java/com/culturalactivities/robin/models/Comment.java
@@ -2,14 +2,29 @@ package com.culturalactivities.robin.models;
 
 public class Comment {
     private User author;
-    private String content, date;
+    private String eventId;
+    private String content, date,id;
     private int rating;
 
-    public Comment(User author, String content, String date, int rating) {
+    public Comment(String id, User author, String eventId, String content, String date, int rating) {
+        this.id=id;
         this.author = author;
+        this.eventId = eventId;
         this.content = content;
         this.date = date;
         this.rating = rating;
+    }
+    public Comment(String id ,String eventId){
+        this.eventId=eventId;
+        this.id=id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public User getAuthor() {
@@ -18,6 +33,14 @@ public class Comment {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
     }
 
     public String getContent() {
--- app/android/Robin/app/src/main/java/com/culturalactivities/robin/utilities/Constants.java
+++ app/android/Robin/app/src/main/java/com/culturalactivities/robin/utilities/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String RATE_URL = "http://139.59.128.92:8080/api/v1/events/rate/";
     public static final String USERS_URL = "http://139.59.128.92:8080/api/v1/users/";
     public static final String PASSWORD_RESET_URL = "http://139.59.128.92:8080/password_reset/";
-    public static final String COMMENT_URL = "comments"; // TODO: 15.12.2018 real link will be placed here
+    public static final String COMMENT_URL = "http://139.59.128.92:8080/api/v1/eventcomments/"; // TODO: 15.12.2018 real link will be placed here
 }
